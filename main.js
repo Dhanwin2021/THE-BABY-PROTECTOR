4fdc0134458b4dcc8e330a8a0e405d9eb962a5e7
Dhanwin2021 <83278381+Dhanwin2021@users.noreply.github.com>
2022-05-23 16:15:56 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+song="";
+status="";
+objects=[];
+
+function preload(){
+    song = loadSound('tornado-siren.mp3');
+}
+
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function draw(){
+    image(video,0,0,380,380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Object Detected";
+           
+                     
+            fill('#FF0000');
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+            if(objects[i].label=="person"){
+                document.getElementById("found").innerHTML="Baby is present...";
+                song.stop();
+            }else{
+                document.getElementById("found").innerHTML="BABY IS NOT HERE!!!!!";
+                song.play();
+                //song.Volume(1);
+            }
+
+        }
+        if(objects.length==0){
+            document.getElementById("found").innerHTML="BABY IS NOT HERE!!!!!";
+            song.play();
+            //song.Volume(1);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
